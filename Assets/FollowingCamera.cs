2f23e66d4e21847eb9e7bd94695fcb036a419da8
Tomáš Malý <tomas.maly@melown.com>
2018-09-24 14:47:19 +0200

    minimap, camera look around, gta hud

--- Assets/FollowingCamera.cs
+++ Assets/FollowingCamera.cs
@@ -4,27 +4,82 @@ public class FollowingCamera : MonoBehaviour
 {
     public GameObject target;
 
-    public Vector3 targetOffset;
-    public float cameraPitch;
-    public bool freezeRotation = false;
+    public Vector3 pivotOffset = new Vector3(0, 2, 0);
+    public float minDistance = 5;
+    public float maxDistance = 50;
+    public float distance = 15;
+    public float minPitch = 0;
+    public float maxPitch = 90;
+    public float pitch = 15;
+    public float yaw = 0;
+    public float cameraMouseSensitivity = 8;
+
+    private float initPitch;
+    private float initYaw;
+    private float initDistance;
+    private float dst;
+
+    private void Ranges()
+    {
+        pitch = Mathf.Clamp(pitch, minPitch, maxPitch);
+        distance = Mathf.Clamp(distance, minDistance, maxDistance);
+    }
+
+    private Vector3 Position()
+    {
+        Quaternion rot = Rotation();
+        Vector3 pivot = target.transform.position + transform.rotation * pivotOffset;
+        RaycastHit hit;
+        if (Physics.Raycast(pivot, rot * new Vector3(0, 0, -1), out hit, distance))
+            dst = Mathf.Max(hit.distance - 0.5f, minDistance);
+        else
+            dst = Mathf.Lerp(dst, distance, 0.1f);
+        return pivot + rot * new Vector3(0, 0, -dst);
+    }
 
     private Quaternion Rotation()
     {
-        if (freezeRotation)
-            return Quaternion.Euler(90, target.transform.rotation.eulerAngles[1], 0);
-        return target.transform.rotation * Quaternion.Euler(cameraPitch, 0, 0);
+        return target.transform.rotation * Quaternion.Euler(pitch, yaw, 0);
     }
 
     private void Start()
     {
+        initPitch = pitch;
+        initYaw = yaw;
+        dst = initDistance = distance;
+        Ranges();
         transform.rotation = Rotation();
-        transform.position = target.transform.position + transform.rotation * targetOffset;
+        transform.position = Position();
     }
 
     void FixedUpdate()
     {
-        Quaternion rot = Rotation();
-        transform.position = Vector3.Lerp(transform.position, target.transform.position + rot * targetOffset, 0.1f);
-        transform.rotation = Quaternion.Lerp(transform.rotation, rot, 0.1f);
+        Ranges();
+        float interp = Cursor.lockState == CursorLockMode.Locked ? 0.3f : 0.1f;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Rotation(), interp);
+        transform.position = Vector3.Lerp(transform.position, Position(), interp);
+    }
+
+    void Update()
+    {
+        if (Cursor.lockState == CursorLockMode.Locked)
+        {
+            yaw += Input.GetAxis("Mouse X") * cameraMouseSensitivity;
+            pitch -= Input.GetAxis("Mouse Y") * cameraMouseSensitivity;
+            distance *= Mathf.Pow(0.1f, Input.GetAxis("Mouse ScrollWheel"));
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Cursor.lockState = Cursor.lockState == CursorLockMode.Locked ? CursorLockMode.None : CursorLockMode.Locked;
+            Cursor.visible = Cursor.lockState == CursorLockMode.None;
+        }
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            pitch = initPitch;
+            yaw = initYaw;
+            distance = initDistance;
+        }
     }
 }
